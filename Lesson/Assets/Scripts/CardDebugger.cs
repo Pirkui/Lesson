722c7ab9d9a388556ffe4feef315a4b64b5c7d00
Nicolas Capelier <capelier.nicolas@gmail.com>
2021-08-03 19:41:49 +0200

    Added scriptable object example

---
+++ Lesson/Assets/Scripts/CardDebugger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDebugger : MonoBehaviour
+{
+    [SerializeField] Card card;
+
+    private void Start()
+    {
+        Debug.Log(card.cardName);
+        Debug.Log(card.lifePoints);
+        Debug.Log(card.damages);
+    }
+}
